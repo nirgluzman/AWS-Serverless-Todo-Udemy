311fcffe62ef9e017daa1b13f775596d1cd762c7
nirgluzman <nir.gluzman@gmail.com>
2023-06-17 12:56:07 +0200

    add getTodo

---
+++ handler/getTodo.js
@@ -0,0 +1,50 @@
+// https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/javascript_dynamodb_code_examples.html
+const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
+const { DynamoDBDocumentClient, GetCommand } = require('@aws-sdk/lib-dynamodb');
+
+const TODOS_TABLE = process.env.TODOS_TABLE;
+const client = new DynamoDBClient();
+const dynamoDbClient = DynamoDBDocumentClient.from(client);
+
+exports.getTodo = async (event, context) => {
+  console.log('event', event);
+  console.log('context', context);
+
+  const { id } = event.pathParameters;
+
+  if (!id) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: 'ID not found',
+      }),
+    };
+  }
+
+  try {
+    const data = await dynamoDbClient.send(
+      new GetCommand({
+        TableName: TODOS_TABLE,
+        Key: { id },
+      })
+    );
+
+    const reponse = data.Item
+      ? {
+          statusCode: 200,
+          body: JSON.stringify(data.Item),
+        }
+      : {
+          statusCode: 404,
+          body: JSON.stringify({ message: 'Todo not dound' }),
+        };
+
+    return reponse;
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error.message),
+    };
+  }
+};
